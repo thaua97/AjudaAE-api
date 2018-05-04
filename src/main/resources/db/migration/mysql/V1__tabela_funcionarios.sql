3c408cd6ae4a89bef1f41cfc0cc88fe83eb72b18
Thauã Borges <thaua97@gmail.com>
2018-05-04 20:48:13 -0300

    :ppo:

--- src/main/resources/db/migration/mysql/V1__tabela_funcionarios.sql
+++ src/main/resources/db/migration/mysql/V1__tabela_funcionarios.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS 'funcionario' (
-    'id' integer (11) NOT NULL,
-    'nome' varchar (60) NOT NULL,
-    'senha' varchar (20) NOT NULL
-    'email' varchar (120) NOT NULL,
-    'cpf' varchar (14) NOT NULL,
-    'endereco' varchar (80) NOT NULL
+CREATE TABLE funcionario (
+    id integer (11) NOT NULL,
+    nome varchar (60) NOT NULL,
+    senha varchar (20) NOT NULL
+    email varchar (120) NOT NULL,
+    cpf varchar (14) NOT NULL,
+    endereco varchar (80) NOT NULL
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE 'funcionario'
-  ADD PRIMARY KEY ('id');
+ALTER TABLE funcionario
+  ADD PRIMARY KEY (id);
 
-ALTER TABLE 'funcionario'
-  MODIFY 'id' integer(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE funcionario
+  MODIFY id integer(11) NOT NULL AUTO_INCREMENT;
