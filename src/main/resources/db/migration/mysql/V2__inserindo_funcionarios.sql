a96778556793a4cf2238b98ad2670aeeaed48089
Thauã Borges <thaua97@gmail.com>
2018-05-04 21:25:45 -0300

    edit migration V2

--- src/main/resources/db/migration/mysql/V2__inserindo_funcionarios.sql
+++ src/main/resources/db/migration/mysql/V2__inserindo_funcionarios.sql
@@ -1,4 +1,4 @@
-INSERT INTO 'funcionario' ('nome', 'email', 'senha', 'cpf', 'endereco')
+INSERT INTO funcionario (nome, email, senha, cpf, endereco)
 VALUES ('Thauã Borges', 'thaua97@gmail.com', '123456', '042.777.610-46', 'Rua 31 de Dezembro, 27');
-INSERT INTO 'funcionario' ('nome', 'email', 'senha', 'cpf', 'endereco')
+INSERT INTO funcionario (nome, email, senha, cpf, endereco)
 VALUES ('Matheus Borges', 'matheus.ads@gmail.com', '123456', '042.777.610-46', 'Rua 31 de Dezembro, 27');
